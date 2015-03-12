bcf8e1563200261e17d464a2d0ee9c253e1fbbda
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2015-03-12 15:15:25 -0400

    Variables for the year and term added

--- scrap.py
+++ scrap.py
@@ -5,16 +5,20 @@ import re
 import mail
 
 course = "ECE419"
+year = "2015"
+term = 1 # Comment this line if the course is a fall one
+term = 9 # Comment this line if the course is a winter one
 from = "a@a.com"
 password = "123456"
 to = "b@utoronto.ca"
+
 
 Lec = True
 Pra = False 
 Tut = False
 
 while True:
-	content = curl.Curl("http://coursefinder.utoronto.ca/course-search/search/courseInquiry?methodToCall=start&viewId=CourseDetails-InquiryView&courseId="+course+"H1S20151").get()
+	content = curl.Curl("http://coursefinder.utoronto.ca/course-search/search/courseInquiry?methodToCall=start&viewId=CourseDetails-InquiryView&courseId="+course+"H1S"+str(year)+str(term)).get()
 	lines = content.split("\r\n")
 	i = lines.index('<table id="u172" class="uif-tableCollectionLayout">')
 	lines = lines[i:]
